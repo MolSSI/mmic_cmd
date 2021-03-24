d9f386fe6bd02be221920ca746089a18f7405370
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-03-24 12:31:51 -0400

    add test for grep

--- mmic_util/tests/test_mmic_utils.py
+++ mmic_util/tests/test_mmic_utils.py
@@ -6,14 +6,19 @@ Unit and regression test for the mmic_util package.
 import mmic_util
 import pytest
 import sys
+import os
 
+cwd = os.getcwd()
 
 def test_mmic_util_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "mmic_util" in sys.modules
 
 
-inp = mmic_util.models.CmdInput(engine="ls", flags=["/", "-ltr"])
-outp = mmic_util.components.CmdComponent.compute(inp)
+def test_mmic_util_ls():
+    inp = mmic_util.models.CmdInput(engine="ls", args=[cwd, "-ltr"])
+    return mmic_util.components.CmdComponent.compute(inp)
 
-print(outp.stdout)
+def test_mmic_util_grep():
+    inp = mmic_util.models.CmdInput(engine="grep", args=["-r", "mmic", cwd])
+    return mmic_util.components.CmdComponent.compute(inp)
