d75b161361a5b5b67ac5528d553ba98783816a5c
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-03-24 16:09:25 -0400

    input schema from qcengine.util

--- mmic_util/models/input.py
+++ mmic_util/models/input.py
@@ -1,22 +1,53 @@
-from mmelemental.models import ProcInput, FileOutput
+from mmelemental.models import ProtoModel, FileOutput
 from pydantic import Field
 from typing import Optional, List, Dict
 
 __all__ = ["CmdInput"]
 
 
-class CmdInput(ProcInput):
-    file_input: List[str] = Field(
+class CmdInput(ProtoModel):
+    """ Input schema for qcengine.execute """
+
+    command: List[str] = Field(..., description=".")
+    infiles: List[str] = Field(
         None,
         description="Input file path(s) or name(s).",
     )
-    file_output: Optional[List[str]] = Field(
+    outfiles: Optional[List[str]] = Field(
         None,
         description="Output file path(s) or name(s).",
     )
-    args: Optional[List[str]] = Field(None, description="Positional command-line arguments or flags. "
-        "Order is preserved. E.g. for the command: 'ls . -ltr', args=['.', '-ltr'].")
-    kwargs: Optional[Dict[str, str]] = Field(
-        None, description="Additional keyword arguments to pass to engine. Order is unimportant. "
-        "E.g. for the command: 'exec --ifile path_to_file', kwargs={'--ifile': path_to_file}."
+    as_binary: Optional[List[str]] = Field(
+        None, description="Keys of `infiles` or `outfiles` to be treated as bytes."
+    )
+    scratch_name: Optional[str] = Field(
+        None, description="Passed to temporary_directory."
+    )
+    scratch_directory: Optional[str] = Field(
+        None, description="Passed to temporary_directory."
+    )
+    scratch_suffix: Optional[str] = Field(
+        None, description="Passed to temporary_directory."
+    )
+    scratch_messy: bool = Field(False, description="Passed to temporary_directory")
+    scratch_exist_ok: bool = Field(False, description="Passed to temporary_directory.")
+    blocking_files: Optional[List[str]] = Field(
+        None, description="Files which should stop execution if present beforehand."
+    )
+    timeout: Optional[int] = Field(
+        None, description="Stop the process after n seconds."
+    )
+    interupt_after: Optional[int] = Field(
+        None, description="Interupt the process (not hard kill) after n seconds."
+    )
+    environment: Optional[Dict[str, str]] = Field(
+        None, description="The environment to run in."
+    )
+    shell: Optional[bool] = Field(False, description="Run command through the shell.")
+    exit_code: Optional[int] = Field(
+        0, description="The exit code above which the process is considered failure."
+    )
+    config: Optional["TaskConfig"] = Field(
+        None,
+        description="Config class for executing tasks. See :class: ``mmic.components.base.config.TaskConfig``.",
     )
