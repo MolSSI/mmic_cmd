e5486a0e834c5080595d04e88c9df4a2ccdccc3e
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-04-08 16:40:23 -0400

    clean docstring

--- mmic_util/components/cmd_component.py
+++ mmic_util/components/cmd_component.py
@@ -9,7 +9,6 @@ __all__ = ["CmdComponent"]
 
 
 class CmdComponent(SpecificComponent):
-    """ Cmd process: build_input() -> run() -> parse_output() -> [clean()] """
 
     @classmethod
     def input(cls):
